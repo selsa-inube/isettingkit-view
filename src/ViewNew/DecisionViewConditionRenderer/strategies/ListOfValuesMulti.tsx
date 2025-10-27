7eea37b29a6b93f9cf5371803fb69c116d911cf7
heber59 <hesan0308@gmail.com>
2025-10-27 15:05:57 -0500

    feat: fromat values $ or % for list data

--- src/ViewNew/DecisionViewConditionRenderer/strategies/ListOfValuesMulti.tsx
+++ src/ViewNew/DecisionViewConditionRenderer/strategies/ListOfValuesMulti.tsx
@@ -1,24 +1,27 @@
+import { formatValue, IRuleDecision } from "@isettingkit/input";
 import { ViewMultipleChoicesNew } from "../../ViewMultipleChoices";
 
 interface IListOfValuesMultiStrategyNew {
   name: string;
   nameLabel: string;
   valueData: string[] | undefined;
+  element: IRuleDecision;
 }
 
 const ListOfValuesMultiStrategyNew = ({
   name,
   nameLabel,
   valueData,
+  element,
 }: IListOfValuesMultiStrategyNew) => {
+  const type = element.decisionDataType! || element.conditionDataType;
   const options = Array.isArray(valueData)
     ? valueData.map((item, index) => ({
         checked: true,
         id: String(index + 1),
-        label: item,
+        label: String(formatValue(item, type)),
       }))
     : [];
-
   return (
     <ViewMultipleChoicesNew id={name} label={nameLabel} options={options} />
   );
